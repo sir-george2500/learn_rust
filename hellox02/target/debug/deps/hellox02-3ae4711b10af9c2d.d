4da9f060645b8cae743ec2d4bb05938657cace93
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-06 19:42:55 +0000

    completed chapter one of the rust book

---
+++ hellox02/target/debug/deps/hellox02-3ae4711b10af9c2d.d
@@ -0,0 +1,5 @@
+/home/sir-george2500/learn_rust/hellox02/target/debug/deps/hellox02-3ae4711b10af9c2d: src/main.rs
+
+/home/sir-george2500/learn_rust/hellox02/target/debug/deps/hellox02-3ae4711b10af9c2d.d: src/main.rs
+
+src/main.rs:
---
+++ hellox02/target/debug/hellox02.d
@@ -0,0 +1,1 @@
+/home/sir-george2500/learn_rust/hellox02/target/debug/hellox02: /home/sir-george2500/learn_rust/hellox02/src/main.rs
