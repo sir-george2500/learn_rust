13b19dfb436b049d913834107570954a9fdd2e41
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-16 16:48:35 +0000

    added a way to get the width and height from the user successfully

--- programming_concept_in_rust/variable_and_mutability/src/main.rs
+++ programming_concept_in_rust/variable_and_mutability/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut width = String::new();
+    let mut length = String::new();
+
+    println!("Please enter the width");
+
+    io::stdin().read_line(&mut width).expect("Failed to read line");
+
+    println!("Please enter the length");
+
+    io::stdin().read_line(&mut length).expect("Failded to readline");
+
+    println!("this is the width {}", width);
+
+    println!("this is the length {}", length);
+
+
+
 }
