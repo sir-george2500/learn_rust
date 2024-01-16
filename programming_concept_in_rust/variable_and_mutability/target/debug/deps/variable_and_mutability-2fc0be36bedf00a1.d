b9aaedec2036f55f95d07e2e093dee88faef4f09
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-16 16:21:02 +0000

    added the general readme

---
+++ programming_concept_in_rust/variable_and_mutability/target/debug/deps/variable_and_mutability-2fc0be36bedf00a1.d
@@ -0,0 +1,5 @@
+/home/sir-george2500/learn_rust/programming_concept_in_rust/variable_and_mutability/target/debug/deps/libvariable_and_mutability-2fc0be36bedf00a1.rmeta: src/main.rs
+
+/home/sir-george2500/learn_rust/programming_concept_in_rust/variable_and_mutability/target/debug/deps/variable_and_mutability-2fc0be36bedf00a1.d: src/main.rs
+
+src/main.rs:
---
+++ programming_concept_in_rust/variable_and_mutability/target/debug/deps/variable_and_mutability-58e113218443f22d.d
@@ -0,0 +1,5 @@
+/home/sir-george2500/learn_rust/programming_concept_in_rust/variable_and_mutability/target/debug/deps/libvariable_and_mutability-58e113218443f22d.rmeta: src/main.rs
+
+/home/sir-george2500/learn_rust/programming_concept_in_rust/variable_and_mutability/target/debug/deps/variable_and_mutability-58e113218443f22d.d: src/main.rs
+
+src/main.rs:
