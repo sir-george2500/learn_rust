34a63fd69a1f747b237ae632a57008fad87e8f35
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-18 12:37:32 +0000

    solve the problem

--- programming_concept_in_rust/functions/src/main.rs
+++ programming_concept_in_rust/functions/src/main.rs
@@ -1,3 +1,22 @@
+fn multiple_of_3_and_5(number:i32) -> i32 {
+
+    let mut sum = 0;
+    let mut i = 1;
+
+    while i <= number {
+
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+        i += 1;
+    }
+    sum
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+   let result =  multiple_of_3_and_5(49);
+
+   println!("This is the result {}", result);
 }
