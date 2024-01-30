39f7364f4fcbbd18c927ad3fa17bc907a64cd003
George S. Mulbah <gsmulbah2500@gmail.com>
2024-01-30 19:10:21 +0000

    added the .gitignore

---
+++ using_struct_to_structual_related_data/defining_and_instantiating_struct/src/main.rs
@@ -0,0 +1,19 @@
+
+// declaring a struct
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    println!("print the user out {}", user1);
+}
