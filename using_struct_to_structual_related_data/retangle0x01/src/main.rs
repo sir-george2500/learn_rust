49af68a1b32b1486e61e1ad6d013a83c05da4e31
George S. Mulbah <gsmulbah2500@gmail.com>
2024-02-01 18:03:02 +0000

    completed rectangle 0x01

---
+++ using_struct_to_structual_related_data/retangle0x01/src/main.rs
@@ -0,0 +1,18 @@
+use std::u32;
+
+
+
+fn main(){
+    let rect = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect)
+        );
+
+}
+
+fn area(dimension:(u32,u32)) -> u32 {
+    dimension.0 * dimension.1
+
+}
