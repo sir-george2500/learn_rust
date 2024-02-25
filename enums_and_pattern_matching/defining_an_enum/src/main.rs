d27fbc3457dd542d19ad08468c355c4e688d5a81
George S. Mulbah <gsmulbah2500@gmail.com>
2024-02-25 01:26:44 +0000

    learn about the message enum

--- enums_and_pattern_matching/defining_an_enum/src/main.rs
+++ enums_and_pattern_matching/defining_an_enum/src/main.rs
@@ -1,24 +1,27 @@
-
-
 /** this is about enum
  *  a enum are just variance
  */
 
 
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+    kind: IpAddrKind,
+    address: String,
 }
 
 
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
+ let home = IpAddrKind::V4(127, 0, 0, 1);
 }
