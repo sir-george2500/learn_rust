db5d315f2a4ae52289d0cd29613309bd4c477fd3
George S. Mulbah <gsmulbah2500@gmail.com>
2024-02-25 03:27:16 +0000

    update the code base

--- using_struct_to_structual_related_data/defining_and_instantiating_struct/src/main.rs
+++ using_struct_to_structual_related_data/defining_and_instantiating_struct/src/main.rs
@@ -1,9 +1,8 @@
-
 // declaring a struct
 #[derive(Debug)]
 struct User {
     active: bool,
-    username: String,
+    user_name: String,
     email: String,
     sign_in_count: u64,
 }
@@ -11,10 +10,23 @@ struct User {
 fn main() {
     let user1 = User {
         active: true,
-        username: String::from("someusername123"),
+        user_name: String::from("someusername123"),
         email: String::from("someone@example.com"),
         sign_in_count: 1,
     };
-
+    let user2 = build_user(
+        String::from("someusername123"),
+        String::from("game@changer.com"),
+    );
     println!("print the user out {:#?}", user1);
+    println!("print the user out {:#?}", user2);
+}
+
+fn build_user(user_name: String, email: String) -> User {
+    User {
+        active: true,
+        user_name,
+        email,
+        sign_in_count: 1,
+    }
 }
