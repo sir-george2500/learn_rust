8334e6647e2f0010957fa676652f92cad27aa908
George S. Mulbah <gsmulbah2500@gmail.com>
2024-03-05 01:46:17 +0000

    added the current code

---
+++ packages_and_crates/restaurant/target/debug/deps/restaurant-7fd1e477c44393be.d
@@ -0,0 +1,5 @@
+/home/sir-george2500/learn_rust/packages_and_crates/restaurant/target/debug/deps/librestaurant-7fd1e477c44393be.rmeta: src/lib.rs
+
+/home/sir-george2500/learn_rust/packages_and_crates/restaurant/target/debug/deps/restaurant-7fd1e477c44393be.d: src/lib.rs
+
+src/lib.rs:
---
+++ packages_and_crates/restaurant/target/debug/deps/restaurant-bd3fc7ea2c8cf52b.d
@@ -0,0 +1,5 @@
+/home/sir-george2500/learn_rust/packages_and_crates/restaurant/target/debug/deps/librestaurant-bd3fc7ea2c8cf52b.rmeta: src/lib.rs
+
+/home/sir-george2500/learn_rust/packages_and_crates/restaurant/target/debug/deps/restaurant-bd3fc7ea2c8cf52b.d: src/lib.rs
+
+src/lib.rs:
